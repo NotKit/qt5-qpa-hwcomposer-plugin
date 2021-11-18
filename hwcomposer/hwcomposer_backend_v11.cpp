38c2865b811cc088afc98caac7674e929d99d6db
Nikita Ukhrenkov <nikita.ukhrenkov@jolla.com>
2021-11-18 17:16:49 +0100

    hack: hwcomposer: use getDisplayConfigs on 1.4 or higher devices
    Rockchip HWC HAL doesn't have getActiveConfig implemented properly on Android 7.x

--- hwcomposer/hwcomposer_backend_v11.cpp
+++ hwcomposer/hwcomposer_backend_v11.cpp
@@ -413,7 +413,9 @@ int HwComposerBackend_v11::getSingleAttribute(uint32_t attribute)
 #ifdef HWC_DEVICE_API_VERSION_1_4
     else {
         /* 1.4 or higher */
-        config = hwc_device->getActiveConfig(hwc_device, 0);
+        // config = hwc_device->getActiveConfig(hwc_device, 0);
+        size_t numConfigs = 1;
+        hwc_device->getDisplayConfigs(hwc_device, 0, &config, &numConfigs);
     }
 #endif
 
